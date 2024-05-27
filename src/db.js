b8c8ec133adfdbd7486d1bec4b9c7dfc1c542e72
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2024-05-27 21:10:42 +0530

    configure sequelize

---
+++ src/db.js
@@ -0,0 +1,12 @@
+// src/db.js
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'sqlite',
+  logging: false,
+});
+
+module.exports = sequelize;
